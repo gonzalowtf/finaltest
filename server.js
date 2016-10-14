cd28fa7e1d6eccdace20b78d50f381b49fa11319
gonzalowtf <gonzalowtf@gmail.com>
2016-10-14 11:54:12 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express"),
+	app = express(),
+	http = require("http"),
+	server = http.createServer(app),
+	port = process.env.PORT || 3000;
+
+
+app.get('/', function(req, res) {
+          //console.log(req.url);  actual direction
+     
+          res.sendfile(__dirname + '/index.html');  
+          //res.end("hello world!");    
+      
+      });
+
+app.use('/files', express.static(__dirname + '/files'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/fonts', express.static(__dirname + '/fonts'));
+app.use('/js', express.static(__dirname + '/js'));
+
+
+server.listen(port,function(err){
+	if(err){
+		console.log("could not start server");
+	}
+	else{
+		console.log("server running at localhost:3000 or online port");
+	}
+});	
