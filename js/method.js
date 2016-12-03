e46c650dba84ecdcfec6a40e17669e083922cda0
Hutter <gonzalowtf@gmail.com>
2016-12-03 11:41:18 -0300

    starting angular

--- js/method.js
+++ js/method.js
@@ -2,10 +2,48 @@
 
 	var app = angular.module('fuzzy',[]);
 
-	app.controller("NewPForm",function(){
-		
+	app.controller("ProblemsController",function(){
+
+		this.problemss = problems;
+
+
 	});
 
+
+
+
+
+
+	var problems = [
+		{
+			name: "problem 1",
+			nd: 5,
+			nc: 4,
+			na: 3,
+			decisors: [
+				{
+					name: "Charly",
+					surname: "Brown",
+					fuzzyRating: "Important",
+					fuzzyValue1 : 2,
+					fuzzyValue2 : 5,
+					fuzzyValue3: 8 
+				}
+			],
+		},
+		{
+			name: "problem 2",
+			nd: 3,
+			nc: 5,
+			na: 9
+		},
+		{
+			name: "problem 3",
+			nd: 10,
+			nc: 6,
+			na: 2
+		}
+	];
 
 
 })();
