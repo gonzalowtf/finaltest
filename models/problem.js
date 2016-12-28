9d8110a423b02b2e88cfcf956925ab74da2c3957
Hutter <gonzalowtf@gmail.com>
2016-12-28 13:37:06 -0300

    decisors and all problems

--- models/problem.js
+++ models/problem.js
@@ -49,5 +49,46 @@ module.exports = mongoose.model('Problem',{
 			fuzzyValue2 : {type : Number , require : true},
 			fuzzyValue3: {type : Number , require : true}
 		}
+	],
+	selectionsCriterias:[
+	{
+		decisorId: {type : String , require : true},
+		criteriaId:{type : String , require : true},
+		comparations:[
+		{
+			criteriaId:{type : String , require : true},
+			fuzzyRating: {type : String,
+				enum: ['Same Importance','Weak Importance','Light Importance','Importance between light and accentuated'
+				,'Importance Accentuated','Strong Importance','Very Strong Importance'
+				,'Extremely Strong Importance','Absolute Importance'],
+				require : true} ,
+			fuzzyValue1 : {type : Number , require : true},
+			fuzzyValue2 : {type : Number , require : true},
+			fuzzyValue3: {type : Number , require : true}
+		}
+					]
+	}
+		
+	],
+	selectionsAlternatives:[
+
+			{
+		decisorId: {type : String , require : true},
+		alternativeId:{type : String , require : true},
+		comparations:[
+		{
+			alternativeId:{type : String , require : true},
+			fuzzyRating: {type : String,
+				enum: ['Extremely Low','Very Low','Low','Medium'
+				,'High','Very High','Extremely High'],require : true} ,
+			fuzzyValue1 : {type : Number , require : true},
+			fuzzyValue2 : {type : Number , require : true},
+			fuzzyValue3: {type : Number , require : true}
+		}
+					]
+	}
+	
+
+
 	]
 });
